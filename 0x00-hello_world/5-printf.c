cb0d94a8d98acb148f7cd5bbab1f2b3a45a1ea79
Understanddeep <davidojoboy164@gmail.com>
2023-09-07 11:08:21 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - using the printf prototype
+ *
+ * Return: always(0) for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0)
+}
