b4d524b6e3e1e32ad3430e4b4b1dec38077fbe06
Understanddeep <davidojoboy164@gmail.com>
2023-09-07 11:01:14 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - my first c  file. to print a string
+ *
+ * Return: always(0) for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
