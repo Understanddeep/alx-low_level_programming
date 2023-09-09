f115bc7e59a1cf3ef928446047a3a96feca141a2
Understanddeep <davidojoboy164@gmail.com>
2023-09-08 22:31:41 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A script to return (1)
+ *
+ * Return: Always (1) (Success)
+ */
+int main(void)
+{ 
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
