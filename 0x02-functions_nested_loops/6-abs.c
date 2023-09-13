d0c60c5030d4d8e82a87703db8c701ffb93cc09b
Understanddeep <davidojoboy164@gmail.com>
2023-09-13 14:57:17 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - A cfile to compute the absolute value of an integer
+ * @i: is the integer to consider
+ * Return: absolute value of integer
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	return (-i);
+}
+
+	
+
