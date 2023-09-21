d7890fb382f0c24032cdf19601905cec376a7622
Understanddeep <davidojoboy164@gmail.com>
2023-09-21 13:50:10 +0100

    Add task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *reset - A function that resets the value of a pointer to 98
+ *@n: is the variable to be declared
+ *
+ */
+
+void reset_to_98(int *n)
+{
+
+	printf("%d", *n = 98);
+}
+
+
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A readme for tasks on pointers, arrays and strings
